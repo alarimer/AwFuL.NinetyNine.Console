3309d5f3e74ac111623f8665455e34ce55c35631
Alan Larimer <alan.larimer@outlook.com>
2025-01-14 05:32:30 -0500

    Create computer opponents

--- Program.cs
+++ Program.cs
@@ -2,12 +2,25 @@
 Console.WriteLine("Ninety Nine - A Card Game");
 
 string playerName;
+Dictionary<int, string> players = [];
 do
 {
     Console.Write("What is your name? ");
     playerName = Console.ReadLine()?.Trim() ?? String.Empty;
 } while (String.IsNullOrEmpty(playerName));
+players.Add(0, playerName);
 Console.WriteLine(Environment.NewLine + $"Hello, {playerName}.");
+
+int computerOpponentCount;
+do
+{
+    Console.Write("How many computer opponents would you like to have: 1 to 4? ");
+} while (!int.TryParse(Console.ReadLine(), out computerOpponentCount) || computerOpponentCount < 1 || computerOpponentCount > 4);
+for (int coc = 1; coc <= computerOpponentCount; coc++)
+{
+    players.Add(coc, $"computer{coc}");
+}
+Console.WriteLine($"Okay, {playerName}, {computerOpponentCount} computer opponents. Good luck!");
 
 Console.WriteLine($"Press any key to exit, {playerName}.");
 Console.ReadKey();
