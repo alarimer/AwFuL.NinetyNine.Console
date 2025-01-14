f66f6ba53e2ceb370fe584fad6e2a763cba66183
Alan Larimer <alan.larimer@outlook.com>
2025-01-14 05:14:31 -0500

    Get the player's name

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Ninety Nine - A Card Game");
+
+string playerName;
+do
+{
+    Console.Write("What is your name? ");
+    playerName = Console.ReadLine()?.Trim() ?? String.Empty;
+} while (String.IsNullOrEmpty(playerName));
+Console.WriteLine(Environment.NewLine + $"Hello, {playerName}.");
+
+Console.WriteLine($"Press any key to exit, {playerName}.");
+Console.ReadKey();
